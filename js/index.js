f09040d79630cfe2aabbc72b2f917d2c08d58ad0
FernandoAgostini28 <fernando.agostini.28@gmail.com>
2021-09-12 14:30:26 -0300

    criar funcao que busca os elementosHTML , cria a url de pesquisa com dia

--- js/index.js
+++ js/index.js
@@ -1,21 +1,49 @@
-const url = 'https://api.nasa.gov/planetary/apod?api_key=';
-const api_key = 'ePZCAgQtjcla5RY1zfF4j3gPSIRJkPATwBL7cyeL';
-const data = '2021-09-11'
+const buscarData = document.getElementById('btnData').addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const input = document.getElementById('inputData').value
+    buscaAPod.criaData(input)
+    buscaAPod.criaUrl();
+})
 
-function retornaFoto() {
-    
+
+
+
+
+
+const buscaAPod = {
+    url: 'https://api.nasa.gov/planetary/apod?api_key',
+    api_key: 'ePZCAgQtjcla5RY1zfF4j3gPSIRJkPATwBL7cyeL',
+    dataEscolhida: '2021-09-11',
+
+
+    criaData: function (data) {
+        this.dataEscolhida = data;
+    },
+
+    criaUrl: function () {
+        fotoDia(this.url, this.api_key, this.dataEscolhida)
+        console.log(this.url, this.api_key, this.dataEscolhida)
+    },
 }
 
-function fotoDia() {
-    try {
-        $.ajax({
 
-            url: url + api_key,
+async function fotoDia(url, api_key, dataEscolhida) {
+    try {
+        await $.ajax({
+
+            url: `${url}=${api_key}&date=${dataEscolhida}`,
             success(resposta) {
-                console.log(resposta)
+                retornaFoto(resposta)
             }
         })
     } catch {
 
     }
 }
+
+function retornaFoto(data) {
+    console.log(data)
+}
+
+
+
